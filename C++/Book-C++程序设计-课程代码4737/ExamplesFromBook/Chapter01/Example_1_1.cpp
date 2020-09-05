a21669747e2780ae287ba9d69ae61c65275fdeb7
Joseph <joseph_wong1993@foxmail.com>
2020-09-06 01:59:21 +0800

    Studying Book-C++程序设计-课程代码4737 Chapter01

--- C++/Book-C++程序设计-课程代码4737/ExamplesFromBook/Chapter01/Example_1_1.cpp
+++ C++/Book-C++程序设计-课程代码4737/ExamplesFromBook/Chapter01/Example_1_1.cpp
@@ -1,28 +1,28 @@
 /* 功能：将结构对象的两个域值相加，乘以2再加50 */
-#include <iostream>	// 包含头文件
-using namespace std;
+#include <iostream>							//包含头文件
+using namespace std;						//使用命名空间
 
-int result(int, int);
-
-const int k = 2;
-struct Point {
-	int x, y;
+int result(int, int);						//result函数的原型声明
+const int k = 2;							//定义常量
+struct Point {								//定义结构point
+	int x, y;								//定义结构成员 x 和 y
 };
 
-int main() {
-	int z(0), b(50);
-	Point a;
-	cout << "输入两个整数（以空格区分）：";
-	cin >> a.x >> a.y;
-	z = (a.x + a.y) * k;
-	z = result(z, b);
-	cout << "计算结果如下" << endl;
-	cout << "（（ " << a.x << " + " << a.y
-		<< " ）* " << k << " ）+ " << b
-		<< " = " << z
-		<< endl;
-	return 0;
-}
+int main()									//主程序
+{											//主程序开始
+	int z(0), b(50);						//初始化整数对象
+	Point a;								//定义结构体对象a
+	cout << "输入两个整数（以空格区分）：";	//输出提示信息
+	cin >> a.x >> a.y;						//接受输入数值
+	z = (a.x + a.y) * k;					//计算结果
+	z = result(z, b);						//计算结果
+	cout << "计算结果如下" << endl;			//输出信息并换行
+	cout << "（（ " << a.x << " + " << a.y	//输出信息
+		<< " ）* " << k << " ）+ " << b		//输出信息
+		<< " = " << z						//输出信息
+		<< endl;							//换行
+	return 0;								//主函数main的返回值
+}											//主函数结束
 
 /// <summary>
 /// 函  数：result
@@ -31,5 +31,5 @@ int main() {
 /// <param name="b">整形对象 b</param>
 /// <returns>整形对象</returns>
 int result(int a, int b) {
-	return a + b;	// 返回 a + b
+	return a + b;							// 返回 a + b
 }
