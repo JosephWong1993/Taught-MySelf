f0ff3f9813159a932aaf99156aac5e0797be37ae
Joseph <joseph_wong1993@foxmail.com>
2020-09-06 23:13:30 +0800

    Studying

---
+++ C++/Book-C++程序设计 课程代码4737/Chapter01/Example_01_02/Example_01_02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int max(int, int);		//2个整数参数的函数原型
+int max(int, int, int);	//3个整数参数的函数原型
+void main() {
+	cout << max(35, 25) << "，" << max(25, 39, 35) << endl;
+}
+int max(int m1, int m2) {
+	return(m1 > m2) ? m1 : m2;
+}
+int max(int m1, int m2, int m3) {
+	int t = max(m1, m2);
+	return max(t, m3);
+}
