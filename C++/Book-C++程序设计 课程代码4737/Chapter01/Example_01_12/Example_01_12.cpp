8bb2d40b10adafb70dbbdf8ba5184902ced9da37
JosephWong <joseph_wong1993@foxmail.com>
2020-09-09 20:01:55 +0800

    Studying Book-C++程序设计 课程代码4737

---
+++ C++/Book-C++程序设计 课程代码4737/Chapter01/Example_01_12/Example_01_12.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+void main() {
+	cout << setfill('*')
+		<< setw(0) << 15 << endl
+		<< setw(1) << 15 << endl
+		<< setw(2) << 15 << endl
+		<< setw(3) << 15 << endl
+		<< setw(4) << 15 << endl;
+	cout << setw(16) << setfill('*') << " " << endl;
+	cout << setiosflags(ios_base::right)
+		<< setw(5) << 1
+		<< setw(5) << 2
+		<< setw(5) << 3 << endl;
+	cout << resetiosflags(ios_base::right);
+	cout << setiosflags(ios_base::left)
+		<< setw(5) << 1
+		<< setw(5) << 2
+		<< setw(5) << 3 << endl;
+}
