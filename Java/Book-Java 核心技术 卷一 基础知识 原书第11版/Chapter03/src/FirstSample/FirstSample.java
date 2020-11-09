bcaf0951428f721d551be1e8aab23b8d636c0304
Joseph <joseph_wong1993@foxmail.com>
2020-11-09 20:38:39 +0800

    Studying

---
+++ Java/Book-Java 核心技术 卷一 基础知识 原书第11版/Chapter03/src/FirstSample/FirstSample.java
@@ -0,0 +1,12 @@
+package FirstSample;
+
+/**
+ * This is the first sample program in Core Java Chapter 3
+ *
+ * @author Joseph Wong
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
