82fe512ee55dac086e951c78d1753424ae2c7956
JosephWong <joseph_wong1993@foxmail.com>
2020-11-13 20:13:23 +0800

    Study

--- Java/黑马程序员-2020/JavaWeb/web01/src/com/wong/servlet/QuickStart.java
+++ Java/黑马程序员-2020/JavaWeb/web01/src/com/wong/servlet/QuickStart.java
@@ -1,31 +1,23 @@
-package com.itheima.servlet;
+package com.wong.servlet;
 
 import javax.servlet.*;
 import java.io.IOException;
 
 /**
  * Servlet技术快速入门
- * 运行在服务器端的程序，称为Servlet，必须在服务器中运行 Tomcat
+ * 运行在服务器端的程序，称为Servlet，必须在服务器中运行
  * 等待客户端访问，连接
  * <p>
  * Servlet技术，客户端交互技术，请求和响应
  * <p>
- * Sun公司，JavaEE平台上的十三个规范之一
+ * Sun攻击，JavaEE平台上的十三个规范之一
  * <p>
  * 定义接口：javax.servlet.Servlet
- * <p>
- * Servlet技术快速入门：
  * 1：创建类，实现接口Servlet
- * 2：重写抽象方法，5个
+ * 2：重写抽象方法，5个（关注一个方法）
  * 3：编写核心配置文件
  */
 public class QuickStart implements Servlet {
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("servlet快速入门");
-        servletResponse.getWriter().println("welcome to ");
-    }
-
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
 
@@ -34,6 +26,11 @@ public class QuickStart implements Servlet {
     @Override
     public ServletConfig getServletConfig() {
         return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
     }
 
     @Override
