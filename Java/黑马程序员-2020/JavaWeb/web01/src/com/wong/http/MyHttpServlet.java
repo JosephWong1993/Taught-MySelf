33a7e6a3d881bf42b11171e0757f961c2190ce17
JosephWong <joseph_wong1993@foxmail.com>
2020-11-16 14:59:02 +0800

    Study

--- Java/黑马程序员-2020/JavaWeb/web01/src/com/wong/http/MyHttpServlet.java
+++ Java/黑马程序员-2020/JavaWeb/web01/src/com/wong/http/MyHttpServlet.java
@@ -19,10 +19,17 @@ import java.io.IOException;
  * MyHttpServlet继承HttpServlet
  * HttpServlet继承GenericServlet
  * GenericServlet实现了接口Servlet
+ * <p>
+ * 结论：
+ * 客户端请求服务器的时候，Tomcat引擎创建MyHttpServlet对象，调用方法service()
+ * 来自于父类的继承，而父类中的service()调用回我们自己的方法doGet()
+ * <p>
+ * 无论什么请求方式，都用一个方法来处理
  */
 public class MyHttpServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("POST");
+        //调用doGET()
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
